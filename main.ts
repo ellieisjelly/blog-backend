9db8f705904026fb9f77dd0506f4ee7ef4b14440
CaioMGT <caio@caiomgt.com>
2023-03-22 21:53:32 -0300

    Initial work for blog backend

--- main.ts
+++ main.ts
@@ -1,13 +1,25 @@
 import {serve} from "https://deno.land/std@0.180.0/http/server.ts";
+function sendJson(json) {
+  return new Response(JSON.stringify(json), {headers:{"Content-Type": "application/json"}})
+}
+function sendJsonWithStatus(json, status) {
+  return new Response(JSON.stringify(json), {headers:{"Content-Type": "application/json"}, status:status})
+}
 async function handler(req: Request): Promise<Response> {
   let abort = false;
   const json = await req.json().catch(() => {
-    console.log("no json lol")
     abort = true
   })
   if (abort) {
-    return new Response("Invalid json, aborting")
+    return sendJsonWithStatus({response:"Invalid json, aborting."}, 400)
   }
-  return new Response("File not found.")
+  switch(json.type) {
+    case "list":
+      return sendJson({response:"noob"})
+      break
+    default:
+      return sendJsonWithStatus({response:"Invalid json, aborting."}, 400)
+  }
+  return sendJsonWithStatus({response:"Could not find file"}, 404)
 }
 serve(handler, { port: 80, hostname:"192.168.1.69" })
