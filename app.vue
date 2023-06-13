7b18b3fcc6680eefe57d509b936b4541faaef686
CaioMGT <caio@caiomgt.com>
2023-06-13 17:51:33 -0300

    Roboto Font

--- app.vue
+++ app.vue
@@ -2,7 +2,16 @@
   <titlebar />
 </template>
 <style>
+@font-face {
+  font-family: Roboto;
+  src: url(/Roboto/Roboto-Regular.ttf);
+}
+@font-face {
+  font-family: Roboto-Thin;
+  src: url(/Roboto/Roboto-Thin.ttf);
+}
 body {
+  font-family: Roboto, Roboto-Thin, sans-serif;
   background-color: rgb(38 38 38);
 }
 </style>
